02f0b798a5301bc448fe08c8b63ed562b65cc11c
Fxker-C <bcontreras0501@outlook.com>
2024-12-27 18:01:22 -0500

    Initial Commit

---
+++ List-MVVM/List_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  List_MVVMApp.swift
+//  List-MVVM
+//
+//  Created by Aly Benjamin Contreras Del Pino on 27/12/24.
+//
+
+import SwiftUI
+
+@main
+struct List_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
